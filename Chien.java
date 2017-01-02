c4b016ad6f14cbeb6f109e7fd0f9c38c739c8784
xablen <xavier.parfait@gmail.com>
2017-01-02 15:24:15 +0100

    utilisation des interface

--- Chien.java
+++ Chien.java
@@ -1,4 +1,4 @@
-public class Chien extends Canin {
+public class Chien extends Canin implements Rintintin {
  
   public Chien(){
 
@@ -11,5 +11,18 @@ public class Chien extends Canin {
 
   void crier() {
     System.out.println("J'aboie sans raison !");
+  } 
+  
+
+  public void faireCalin() {
+    System.out.println("Je te fais un GROS CÂLIN");               
+  }
+
+  public void faireLeBeau() {
+    System.out.println("Je fais le beau !");
+  }
+
+  public void faireLechouille() {
+    System.out.println("Je fais de grosses léchouilles...");
   } 
 }
--- Test.java
+++ Test.java
@@ -6,5 +6,26 @@ public class Test {
     l.deplacement();
     l.crier();
     System.out.println(l.toString());
+    
+    //Les méthodes d'un chien 
+    Chien c = new Chien("Gris bleuté", 20);
+    c.boire();
+    c.manger();
+    c.deplacement();
+    c.crier();
+    System.out.println(c.toString());
+			
+    System.out.println("--------------------------------------------");
+    //Les méthodes de l'interface
+    c.faireCalin();
+    c.faireLeBeau();
+    c.faireLechouille();
+		
+    System.out.println("--------------------------------------------");
+    //Utilisons le polymorphisme de notre interface
+    Rintintin r = new Chien();
+    r.faireLeBeau();
+    r.faireCalin();
+    r.faireLechouille();
   } 
 }
